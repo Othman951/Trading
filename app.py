bd19ba4022187b3490dad9681060bc5701e86839
Othman951 <ot.belfakir@gmail.com>
2025-05-25 00:33:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-# app.py
+import os
 from flask import Flask
-from routes import main  # Import du blueprint défini dans routes.py
+from routes import main
 
 app = Flask(__name__)
-app.register_blueprint(main)  # Enregistrement du blueprint
+app.register_blueprint(main)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Récupère le port fourni par Render ou 5000 par défaut
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
